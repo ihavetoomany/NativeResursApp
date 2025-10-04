84c6354dc645edc5ab76f04f1dcc3cb7f3888b35
Bjarne <bjarne@likabra.se>
2025-10-04 20:33:40 +0200

    Initial Commit

---
+++ NativeResursApp/NativeResursAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NativeResursAppApp.swift
+//  NativeResursApp
+//
+//  Created by Bjarne Werner on 2025-10-04.
+//
+
+import SwiftUI
+
+@main
+struct NativeResursAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NativeResursAppTests/NativeResursAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  NativeResursAppTests.swift
+//  NativeResursAppTests
+//
+//  Created by Bjarne Werner on 2025-10-04.
+//
+
+import Testing
+@testable import NativeResursApp
+
+struct NativeResursAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
